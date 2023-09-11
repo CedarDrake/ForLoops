2bb72016e5e455de2d36ca52115ecea930ca0582
Cedar Drake <cedar.drake@mrjaffesclass.com>
2023-09-11 11:36:14 -0700

    Fixed

--- ForLoops/ForLoops.java
+++ ForLoops/ForLoops.java
@@ -6,22 +6,20 @@ public class ForLoops
     int d = -8;
     public void run() {
         int i = 0;
-        while (i < 15) {
+        do {
             System.out.println("i is "+ i + " but is less than 10");
             i += 1;
-        }
+        } while (i < 15);
         int k = 20;
-        while (k > 5) {
+        do {
             System.out.println("k is "+ k +" but is greater than 5");
             k -= 1;
-        }
-        while (a < 10) {
-            while (b <= 20) {
-                System.out.println("a is "+ a +" but is less than 10 AND b is "+ b +" But is less than or equal to 20");
-                a++;
-                b++;
-            }
-        }
+        } while (k > 5);
+        do {
+            System.out.println("a is "+ a +" but is less than 10 AND b is "+ b +" But is less than or equal to 20");
+            a++;
+            b++;
+        } while ((a < 10) && (b <= 20));
     }
 }
     
